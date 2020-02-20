89c38db187b2c7d1afac423a3149568d49027f88
mengyi-qian <mengyi.qian@yale.edu>
2020-02-19 20:40:47 -0500

    44
    444

--- project-2/assets/script.js
+++ project-2/assets/script.js
@@ -1,92 +1,26 @@
+var button = document.querySelector('button')
 
+button.addEventListener('click', () => {
+  DeviceOrientationEvent.requestPermission()
+    .then(response => {
+      if (response == 'granted') {
+        window.addEventListener('deviceorientation', (event) => {
+          console.log(e);
+          // alpha: rotation around z-axis
+          var rotateDegrees = event.alpha;
+          // gamma: left to right
+          var leftToRight = event.gamma;
+          // beta: front back motion
+          var frontToBack = event.beta;
 
-DeviceMotionEvent.requestPermission()
-.then(response => {
-  if (response == 'granted') {
-    window.addEventListener('devicemotion', (e) => {
-      // do something with e
+          handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+        })
+      }
+
+      var handleOrientationEvent = function(frontToBack, leftToRight, rotateDegrees) {
+        sphere.style.top = frontToBack;
+    		sphere.style.left = leftToRight;
+      };
     })
-  }
+    .catch(console.error)
 })
-.catch(console.error)
-
-
-DeviceOrientationEvent.requestPermission()
-.then(response => {
-  if (response == 'granted') {
-    window.addEventListener('deviceorientation', (e) => {
-      // do something with e
-    })
-  }
-})
-.catch(console.error)
-
-
-let accelerometer = new Accelerometer({frequency: 60});
-
-accelerometer.addEventListener('reading', e => {
-  console.log("Acceleration along the X-axis " + accelerometer.x);
-  console.log("Acceleration along the Y-axis " + accelerometer.y);
-  console.log("Acceleration along the Z-axis " + accelerometer.z);
-});
-accelerometer.start();
-
-
-
-
-
-
-
-
-
-var x = 0, y = 0,
-    vx = 0, vy = 0,
-	  ax = 0, ay = 0;
-
-var sphere = document.getElementById("sphere");
-
-if (window.DeviceMotionEvent != undefined) {
-	window.ondevicemotion = function(e) {
-		ax = event.accelerationIncludingGravity.x * 5;
-		ay = event.accelerationIncludingGravity.y * 5;
-		document.getElementById("accelerationX").innerHTML = e.accelerationIncludingGravity.x;
-		document.getElementById("accelerationY").innerHTML = e.accelerationIncludingGravity.y;
-		document.getElementById("accelerationZ").innerHTML = e.accelerationIncludingGravity.z;
-
-		if ( e.rotationRate ) {
-			document.getElementById("rotationAlpha").innerHTML = e.rotationRate.alpha;
-			document.getElementById("rotationBeta").innerHTML = e.rotationRate.beta;
-			document.getElementById("rotationGamma").innerHTML = e.rotationRate.gamma;
-		}
-	}
-
-	setInterval( function() {
-		var landscapeOrientation = window.innerWidth/window.innerHeight > 1;
-		if ( landscapeOrientation) {
-			vx = vx + ay;
-			vy = vy + ax;
-		} else {
-			vy = vy - ay;
-			vx = vx + ax;
-		}
-		vx = vx * 0.98;
-		vy = vy * 0.98;
-		y = parseInt(y + vy / 50);
-		x = parseInt(x + vx / 50);
-
-		boundingBoxCheck();
-
-		sphere.style.top = y + "px";
-		sphere.style.left = x + "px";
-
-	}, 25);
-}
-
-
-function boundingBoxCheck(){
-	if (x<0) { x = 0; vx = -vx; }
-	if (y<0) { y = 0; vy = -vy; }
-	if (x>document.documentElement.clientWidth-20) { x = document.documentElement.clientWidth-20; vx = -vx; }
-	if (y>document.documentElement.clientHeight-20) { y = document.documentElement.clientHeight-20; vy = -vy; }
-
-}
--- project-2/assets/script2.js
+++ project-2/assets/script2.js
@@ -1,9 +1,3 @@
-var x = 0, y = 0,
-    vx = 0, vy = 0,
-	ax = 0, ay = 0;
-
-var sphere = document.getElementById("sphere");
-
 var button = document.querySelector('button')
 
 button.addEventListener('click', () => {
@@ -17,24 +11,3 @@ button.addEventListener('click', () => {
     })
     .catch(console.error)
 })
-
-
-
-	setInterval( function() {
-		var landscapeOrientation = window.innerWidth/window.innerHeight > 1;
-		if ( landscapeOrientation) {
-			vx = vx + ay;
-			vy = vy + ax;
-		} else {
-			vy = vy - ay;
-			vx = vx + ax;
-		}
-		vx = vx * 0.98;
-		vy = vy * 0.98;
-		y = parseInt(y + vy / 50);
-		x = parseInt(x + vx / 50);
-
-		sphere.style.top = y + "px";
-		sphere.style.left = x + "px";
-
-	}, 25);
